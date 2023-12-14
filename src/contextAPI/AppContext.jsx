ea8c468dbf98888094600478389638eb653b9c85
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-14 16:26:25 +0530

    loginstatus, formstatus and usedetails

--- src/contextAPI/AppContext.jsx
+++ src/contextAPI/AppContext.jsx
@@ -5,10 +5,11 @@ export const AppContext = createContext();
 const AppContextProvider = ({ children }) => {
     const [loggedin, setLoggedIn] = useState(false);
     const [user,setUser] = useState(null);
+    const [form,setForm] = useState("login");
 
     const value = {
         loggedin,setLoggedIn,
-        user,setUser
+        user,setUser,form ,setForm
     }
 
     return <AppContext.Provider value={value}>
