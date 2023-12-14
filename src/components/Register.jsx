bdf6a78311293f7bbcb00de8471dae97bd8a7984
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-14 16:24:27 +0530

    register  code

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useForm} from "react-hook-form";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom"
+import { AppContext } from "../contextAPI/AppContext";
+import { backend_url } from "../contant";
+import Login from "./Login";
 
-const Register = () => {
+export default function Register() {
+  const { register, handleSubmit } = useForm()
+  const navigate = useNavigate();
+
+  const {setLoggedIn,setUser,setForm} = useContext(AppContext);
+
+  const submitHandler = async (loginData) => {
+    
+    console.log(loginData)
+    try{
+      const res = await fetch(backend_url+"/register",{
+        method : "post",
+        headers : {"content-type" : "application/json"},
+        body : JSON.stringify(loginData)
+      })
+      const result = await res.json();
+      console.log(result);
+      if (res.status === 200) {
+        setLoggedIn(true);
+        setForm("login");
+      } else if (res.status === 404) {
+        setLoggedIn(false);
+      } else {
+        setLoggedIn(false);
+      }
+
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+
   return (
-    <div>Register</div>
+    <div className="w-full bg-white ">
+      <h1 className="mb-5">Register</h1>
+    <form onSubmit={handleSubmit(submitHandler)}>
+    <div>
+        <input {...register("Name")} type="text" placeholder="Name" className="w-full px-2  py-1 border border-gray-500 my-2"/>
+      </div>
+      <div>
+        <input {...register("Email")} type="email" placeholder="Email" className="w-full px-2  py-1 border border-gray-500"/>
+      </div>
+      <div>
+        <input {...register("Password" )} type="text" placeholder="password"  className="w-full px-2 my-2  py-1 border border-gray-500"></input>
+      </div>
+      
+      <input type="submit" value={"sign in"} className="w-full py-2 bg-sky-500 text-white"/>
+    </form>
+    <div className="flex gap-4 justify-center mt-2">
+        <p className="underline italic font-light " onClick={() => navigate("/passwordUpdate")}>forgot password</p>
+        <p className="underline italic font-light " onClick={() => setForm("login")}>login</p>
+      </div>
+    </div>
   )
 }
-
-export default Register
