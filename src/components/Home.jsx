df05ff36c0625a4f50a25a41c16d351e0143722a
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-14 21:55:04 +0530

    regiter or login or dashboard conditions added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,28 @@
-import React, { useContext } from 'react'
-import Login from './Login';
-import { AppContext } from '../contextAPI/AppContext';
-import Register from './Register';
+import React, { useContext } from "react";
+import Login from "./Login";
+import { AppContext } from "../contextAPI/AppContext";
+import Register from "./Register";
+import Dashboard from "./Dashboard";
 export const Home = () => {
-  const {form} = useContext(AppContext);
+  const { form } = useContext(AppContext);
 
   return (
-    <div className='flex flex-col items-center gap-2 py-10 bg-gray-300 h-screen'>
-      <h1 className='font-bold text-3xl mb-5 '>Jala Academy</h1>
-      <h2 className=' text-3xl capitalize'>Login credentials </h2>
-      <h3>Email : training@jalaacademy.com</h3>
-      <h3>Password : jobprogram</h3>
-      <div className='w-1/2 bg-white p-5'>
-        {
-          form === "login" ? <Login></Login> : <Register/>
-        }
-        
-      </div>
+    <div className="flex flex-col items-center gap-2 py-10 bg-gray-300 h-screen">
+      <h1 className="font-bold text-3xl mb-5 ">Jala Academy</h1>
+
+      {form === "userData" ? (
+        <Dashboard />
+      ) : (
+        <div className="w-full flex flex-col items-center gap-2">
+          <h2 className=" text-3xl capitalize">Login credentials </h2>
+          <h3>Email : training@jalaacademy.com</h3>
+          <h3>Password : jobprogram</h3>
+          <div className="w-1/2 bg-white p-5">
+            {form === "login" ? <Login /> : <Register />}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 export default Home;
