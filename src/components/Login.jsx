3536ce84eedf17794f4fadde36eb1c543ce2fdfb
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-15 21:51:16 +0530

    toast added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,64 +1,112 @@
-import { useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { AppContext } from "../contextAPI/AppContext";
-
+import toast from 'react-hot-toast';
 
 export default function Login() {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
 
-  const {setLoggedIn,setUser,setForm} = useContext(AppContext);
+  const { setLoggedIn, setUser, form, setForm } = useContext(AppContext);
 
   const submitHandler = async (loginData) => {
-    
-    console.log(loginData)
-    try{
-      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/login`,{
-        method : "post",
-        headers : {"content-type" : "application/json"},
-        body : JSON.stringify(loginData)
-      })
+
+    let toastid;
+
+    console.log(loginData);
+    try {
+      toastid = toast.loading("login in process...")
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+        method: "post",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(loginData),
+      });
       const result = await res.json();
       console.log(result);
 
       if (res.status === 200) {
         setLoggedIn(true);
+
+        //toast
+        toast.success("login successfull",{id : toastid});
+
+        //set user details in var and localstorage
         setUser(JSON.stringify(result.user));
-        localStorage.setItem("user",JSON.stringify(result.user));
-        console.log(JSON.parse(localStorage.getItem('user')));  
+        localStorage.setItem("user", JSON.stringify(result.user));
+
+        //form status change for dashboard screen
         setForm("userData");
+
+        //navigate to home
         navigate("/");
       } else if (res.status === 404) {
+        toast.error(result?.message, {id : toastid});
+
         setLoggedIn(false);
       } else {
+        toast.error(result?.message, {id : toastid});
         setLoggedIn(false);
       }
-
-    }catch(err){
+    } catch (err) {
+      toast.error(err?.message, {id : toastid});
       console.log(err);
     }
-  }
-
+  };
 
   return (
     <div className="w-full bg-white ">
       <h1 className="mb-5">Sign in</h1>
-    <form onSubmit={handleSubmit(submitHandler)}>
-      <div>
-        <input {...register("Email")} type="email" placeholder="Email" className="w-full px-2  py-1 border border-gray-500"/>
-      </div>
-      <div>
-        <input {...register("Password" )} type="text" placeholder="password"  className="w-full px-2 my-2  py-1 border border-gray-500"></input>
-      </div>
-      
-      <input type="submit" value={"sign in"} className="w-full py-2 bg-sky-500 text-white"/>
-    </form>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <div>
+          <input
+            {...register("Email")}
+            type="email"
+            placeholder="Email"
+            className="w-full px-2  py-1 border border-gray-500"
+          />
+        </div>
+        <div>
+          <input
+            {...register("Password")}
+            type="text"
+            placeholder="password"
+            className="w-full px-2 my-2  py-1 border border-gray-500"
+          ></input>
+        </div>
+
+        <input
+          type="submit"
+          value={"sign in"}
+          className="w-full py-2 bg-sky-500 text-white"
+        />
+      </form>
       <div className="flex gap-4 justify-center mt-2">
-        <p className="underline italic font-light " onClick={() => navigate("/passwordUpdate")}>forgot password</p>
-        <p className="underline italic font-light " onClick={() => setForm("register")}>Register</p>
+        <p
+          className="underline italic font-light "
+          onClick={() => navigate("/passwordUpdate")}
+        >
+          forgot password
+        </p>
+        <p
+          className="underline italic font-light "
+          onClick={() => {
+            setForm("register");
+            console.log(form + " register");
+          }}
+        >
+          Register
+        </p>
+        <p
+          className="underline italic font-light "
+          onClick={() => {
+            setForm("userData");
+            console.log(form + "see users");
+          }}
+        >
+          See Users
+        </p>
       </div>
-      
     </div>
-  )
+  );
 }
