ea5832415c0869a9d35e0a12603e426b375cec10
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-15 22:05:20 +0530

    email,form and user added

--- src/contextAPI/AppContext.jsx
+++ src/contextAPI/AppContext.jsx
@@ -4,12 +4,15 @@ export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
     const [loggedin, setLoggedIn] = useState(false);
-    const [user,setUser] = useState(null);
-    const [form,setForm] = useState("login");
-
+    const [email,setEmail] = useState({});
+    const [user,setUser] = useState(JSON.parse(localStorage.getItem('user')) || null);
+    const [form,setForm] = useState(localStorage.getItem("user") == null ? "login" : "userData");
+    
+    
     const value = {
         loggedin,setLoggedIn,
-        user,setUser,form ,setForm
+        user,setUser,form ,setForm,
+        email,setEmail
     }
 
     return <AppContext.Provider value={value}>
