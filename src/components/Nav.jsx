24a2bd8dfbcad2b58d1a1e450dfc5dfeaba58c26
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-15 22:09:25 +0530

    used AppContext

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { useContext } from 'react';
 import { AppContext } from '../contextAPI/AppContext';
 const Nav = () => {
     const navigate = useNavigate();
-    const {loggedin,setLoggedIn} = useContext(AppContext);
+    const {loggedin,setLoggedIn,setForm} = useContext(AppContext);
     console.log(loggedin);
   return (
     <div className='flex gap-5 p-4 justify-evenly'>
@@ -18,10 +18,10 @@ const Nav = () => {
         </div>
         {/* buttons  */}
         <div className='space-x-5'>{
-            !loggedin ? <span onClick={() => {navigate("/login"); setLoggedIn(true)}} className='px-3 py-2 border cursor-pointer'>Login </span> : <span onClick={() => {navigate("/"); setLoggedIn(false)}} className='px-3 py-2 border cursor-pointer'>Logout </span>
+            !loggedin ? <span onClick={() => {navigate("/login");}} className='px-3 py-2 border cursor-pointer'>Login </span> : <span onClick={() => {setForm("login"); setLoggedIn(false)}} className='px-3 py-2 border cursor-pointer'>Logout </span>
         }
             
-            <span onClick={() => navigate("/register")} className='px-3 py-2 border cursor-pointer'>Register</span>
+            <span onClick={() => setForm("register")} className='px-3 py-2 border cursor-pointer' >Register</span>
         </div>
     </div>
   )
