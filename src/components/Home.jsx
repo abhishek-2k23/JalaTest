c06388c8741dc06fe42b4f6a2dd6526b0ba66e43
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-15 21:53:10 +0530

    toast added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,11 @@ import Login from "./Login";
 import { AppContext } from "../contextAPI/AppContext";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
+import toast from "react-hot-toast";
+
 export const Home = () => {
   const { form } = useContext(AppContext);
-
+  console.log(form);
   return (
     <div className="flex flex-col items-center gap-2 py-10 bg-gray-300 h-screen">
       <h1 className="font-bold text-3xl mb-5 ">Jala Academy</h1>
@@ -18,7 +20,8 @@ export const Home = () => {
           <h3>Email : training@jalaacademy.com</h3>
           <h3>Password : jobprogram</h3>
           <div className="w-1/2 bg-white p-5">
-            {form === "login" ? <Login /> : <Register />}
+            {form === "login" && <Login />}
+            {form === "register" && <Register />}
           </div>
         </div>
       )}
